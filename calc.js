c35ec27364e5fe2a1e024b15ca0e70c44cfed384
alanbixby <alanrbixby@gmail.com>
2020-06-03 03:03:02 -0400

    Added 'Current Level' input as the !rank command no longer displays total XP.

--- calc.js
+++ calc.js
@@ -6,11 +6,13 @@ function calculate()
 
     //Input from user
     var desired_level = document.getElementById("desired-level").value;
+    var current_level = document.getElementById("current-level").value;
     var current_xp = document.getElementById("current-xp").value;
 
     //Magical math functions
     var xp_to_desired_level = 5 / 6 * desired_level * (2 * desired_level * desired_level + 27 * desired_level + 91);
-    var xp_needed = xp_to_desired_level - current_xp;
+    var xp_of_current_level = 5 / 6 * current_level * (2 * current_level * current_level + 27 * current_level + 91);
+    var xp_needed = xp_to_desired_level - xp_of_current_level - current_xp;
 
     //Minimum, average, and maximum messages needed to send in order to reach specified level
     var min_messages_needed_to_send = Math.ceil(xp_needed / max_xp_gain_per_message);
