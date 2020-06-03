328d17c9228f9477012975fc3224b7f0d6ccac7a
alanbixby <alanrbixby@gmail.com>
2020-06-03 04:49:49 -0400

    Cleaned up interface, converted messages -> minutes, human readable durations added, README improved.

--- README.md
+++ README.md
@@ -1,18 +1,28 @@
-# mee6calc.xyz _(For [mee6.xyz](https://mee6.xyz)-powered servers)_
+# **[MEE6 Calculator](https://alanbixby.github.io/mee6calc/)**
+### A fork of vladdeSV's archived 'mee6calc.xyz'.
 ![screenshot](http://i.imgur.com/nHu5NZF.png)
 Calculate how many more messages you need to send (for every minute) in order to reach a specific level!
 
 ### Usage
-0. Send `!rank` in a server where a Mee6 bot is present.
-0. Get your TOTAL XP (example output: ` LEVEL 14 | XP 766/1780 | TOTAL XP 10811 | Rank 21/1089`)
-0. Go to [mee6calc.xyz](http://mee6calc.xyz)
-  1. Enter the level you want to reach
-  1. Enter your total XP
-  1. Press _Calculate_
-0. Evaluate information you have received.
+1. Use the command `!rank` in a server where a MEE6 bot is present.
+1. Take note of your `Current Level` and the  `Current XP` displayed. ![screenshot](https://i.gyazo.com/d30658033449d2b9547c5739a3995ea0.png)
+1. Go to the **[MEE6 Calculator](https://alanbixby.github.io/mee6calc/)**.
+  1. Enter the `Desired Level` you want to reach.
+  1. Enter your `Current Level`.
+  1. Enter your `Current XP` displayed for that level.
+  1. Press **`Calculate`**.
+1. Read the output under `Result`.
 
-### But how does it work...?
-The leveling system follows a pattern, which can be explained with this mathematical function: ![function](http://i.imgur.com/GX5ae4A.jpg), where `x` is the level desired, and `f(x)` is the XP required for specified level. Subtract your current XP from the required XP, and what is left is how much more is needed until that level is reached. Since every message (per minute) gives [between 15 and 25 XP](https://github.com/cookkkie/mee6/blob/5da379573c06eddec8ffad455c5b10681da429c3/chat-bot/plugins/levels.py#L173), it is averaged to about 20 XP per message.
+---
+### **But how does it work...?**
+MEE6's leveling system follows a pattern, which can be explained with a mathematical function, where `x` is the level desired, and `f(x)` is the XP required for specified level.
 
-##### Disclaimer
-I am not part of the [mee6.xyz](https://mee6.xyz) development team. This project is entirely made for fun.
+![function](https://i.imgur.com/QsV6E8N.png)
+> The top and bottom functions are equivalent.
+
+Using this, we can calculate your total XP by plugging your current level into this formula and adding your visible XP. Then, we subtract your total XP from the required XP, and what is left is how much more is needed until that level is reached. 
+
+Then, since every message (per minute) gives [between 15 and 25 XP](https://github.com/cookkkie/mee6/blob/5da379573c06eddec8ffad455c5b10681da429c3/chat-bot/plugins/levels.py#L173), it is averaged to about 20 XP per message; and we can also calculate the best and worst case scenarios.
+
+---
+##### Disclaimer: This project is not associated with the development team of [MEE6.xyz](https://mee6.xyz) and is merely a personal-use utility.
--- calc.js
+++ calc.js
@@ -1,27 +1,46 @@
-function calculate()
-{
+function calculate() {
     //Constants
-    var min_xp_gain_per_message = 15;
-    var max_xp_gain_per_message = 25;
+    let min_xp_gain_per_message = 15;
+    let max_xp_gain_per_message = 25;
 
     //Input from user
-    var desired_level = document.getElementById("desired-level").value;
-    var current_level = document.getElementById("current-level").value;
-    var current_xp = document.getElementById("current-xp").value;
+    let desired_level = document.getElementById("desired-level").value;
+    let current_level = document.getElementById("current-level").value;
+    let current_xp = document.getElementById("current-xp").value;
 
-    //Magical math functions
-    var xp_to_desired_level = 5 / 6 * desired_level * (2 * desired_level * desired_level + 27 * desired_level + 91);
-    var xp_of_current_level = 5 / 6 * current_level * (2 * current_level * current_level + 27 * current_level + 91);
-    var xp_needed = xp_to_desired_level - xp_of_current_level - current_xp;
+    //Math functions to determine XP requirements
+    let xp_to_desired_level = 5 / 6 * desired_level * (2 * desired_level * desired_level + 27 * desired_level + 91);
+    let xp_of_current_level = 5 / 6 * current_level * (2 * current_level * current_level + 27 * current_level + 91);
+    let xp_needed = Math.round(xp_to_desired_level - xp_of_current_level - current_xp);
 
     //Minimum, average, and maximum messages needed to send in order to reach specified level
-    var min_messages_needed_to_send = Math.ceil(xp_needed / max_xp_gain_per_message);
-    var avg_messages_needed_to_send = Math.ceil(xp_needed / ((min_xp_gain_per_message + max_xp_gain_per_message) / 2));
-    var max_messages_needed_to_send = Math.ceil(xp_needed / min_xp_gain_per_message);
+    let min_messages_needed_to_send = Math.ceil(xp_needed / max_xp_gain_per_message);
+    let avg_messages_needed_to_send = Math.ceil(xp_needed / ((min_xp_gain_per_message + max_xp_gain_per_message) / 2));
+    let max_messages_needed_to_send = Math.ceil(xp_needed / min_xp_gain_per_message);
 
     //Display information to user
-    document.getElementById("result-average").innerHTML = avg_messages_needed_to_send;
+    document.getElementById("total-xp").innerHTML = xp_of_current_level - (-1 * current_xp);
+    document.getElementById("result-average").innerHTML = minutesToString(avg_messages_needed_to_send);
     document.getElementById("result-xp-needed").innerHTML = xp_needed;
     document.getElementById("result-minimum-messages-requirement").innerHTML = min_messages_needed_to_send;
     document.getElementById("result-maximum-messages-requirement").innerHTML = max_messages_needed_to_send;
+}
+
+function minutesToString(minutes) {
+    if (minutes == 0) return '0 minutes';
+
+    let timeIncrement = [
+        [Math.floor(minutes / 525600), 'years'],
+        [Math.floor((minutes % 525600) / 1440), 'days'],
+        [Math.floor(((minutes % 525600) % 1440) / 60), 'hours'],
+        [Math.floor(((minutes % 525600) % 1440) % 60), 'minutes']
+    ]
+    let retVal = '';
+
+    for (let i = 0; i < timeIncrement.length; i++) {
+        if (timeIncrement[i][0] === 0) continue;
+        retVal += ' ' + timeIncrement[i][0] + ' ' + (timeIncrement[i][0] === 1 ? timeIncrement[i][1].substr(0, timeIncrement[i][1].length-1): timeIncrement[i][1]) + ",";
+    }
+
+    return retVal.substring(0, retVal.length - 1).trim();
 }
